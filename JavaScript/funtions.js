f727b7052e6b9a569c095f3c5cf87ee8d61cf6e3
myaaaaaaaaaaaa111233434 <mya.vazquezvillepvr21@gmail.com>
2024-02-09 12:10:04 -0600

    agregue un formulario jiji

--- JavaScript/funtions.js
+++ JavaScript/funtions.js
@@ -1,3 +1,9 @@
 function changeColor(){
   document.getElementById("palabrita").style.backgroundColor="pink";
+
+  document.getElementById("palabrita").textContent="Holiszzz";
+}
+
+  function enviarMsg(){
+    alert("Hola amigo");
 }
